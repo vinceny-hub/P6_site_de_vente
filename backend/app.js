253ed6a98ff908614a75482131d1a6d58e66201c
vinceny <vinceny@live.fr>
2021-03-04 00:16:45 +0300

    P6_site_de_vente

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const  bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const path = require('path')
+
+const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
+
+
+const app = express()
+
+mongoose.connect('mongodb+srv://P6-cours:OLw79b9A8wbCkDJE@cluster0.8hk79.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json())
+
+app.use('/images', express.static(path.join(__dirname,'images')))
+
+app.use('/api/stuff', stuffRoutes)
+app.use('/api/auth', userRoutes)
+
+
+module.exports = app
